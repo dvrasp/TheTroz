813c636faee267901c48e54334edf82a42a58c80
Peter Naudus <linuxlefty@fastmail.fm>
2013-02-28 01:00:34 -0500

    Added tests for spells

---
+++ spells/ddg/test.py
@@ -0,0 +1,42 @@
+from lib.test import Shaman
+
+class DDG(Shaman):
+
+    def setUp(self):
+        # Register URLs
+        self.web.route(
+            url='http://api.duckduckgo.com',
+            get={'q': 'George Washington', 'no_html': 1, 'no_redirect': 1, 'skip_disambig': 1, 'format': 'json'},
+            format='json',
+            file='george_washington.json'
+        )
+
+        self.web.route(
+            url='http://api.duckduckgo.com',
+            get={'q': 'cookies', 'no_html': 1, 'no_redirect': 1, 'skip_disambig': 1, 'format': 'json'},
+            format='json',
+            file='cookies.json'
+        )
+
+        self.web.route(
+            url='http://api.duckduckgo.com',
+            get={'q': '13 + 14', 'no_html': 1, 'no_redirect': 1, 'skip_disambig': 1, 'format': 'json'},
+            format='json',
+            file='13_plus_14.json'
+        )
+
+    def test_george_washington(self):
+        result = self.query("Who is George Washington?")
+        output = "George Washington was one of the Founding Fathers of the United States, serving as the commander-in-chief of the Continental Army during the American Revolutionary War."
+        self.assertLooksLike(result, output)
+
+    def test_cookies(self):
+        result = self.query("What are cookies?")
+        output = "A small, usually flat and crisp cake made from sweetened dough."
+        self.assertLooksLike(result, output)
+
+    def test_13_plus_14(self):
+        # Run a test, check the output and state
+        result = self.query("What is 13 + 14?")
+        output = "13 + 14 = 27"
+        self.assertLooksLike(result, output)
---
+++ spells/wolframAlpha/test.py
@@ -0,0 +1,33 @@
+from lib.test import Shaman
+
+queries = [
+    ("What is the tallest building in the world?", "Burj Khalifa (2717 feet)", "tallest_building.xml"),
+    ("Where was George Washington born?", "Westmoreland County, Virginia", "george_washington_birthplace.xml"),
+    ("When is Easter?", "Sunday, March 31, 2013", "easter_date.xml"),
+    ("How many cups are in a gallon?", "16 cups", "cups_in_gallon.xml"),
+    ("How much is 15 miles in feet?", "79200 feet", "15miles_in_feet.xml"),
+    ("15 USD in RMB", "yuan93.44  (Chinese yuan)", "15USD_in_RMB.xml")
+]
+    
+    
+class WolframAlpha(Shaman):
+
+    def setUp(self):
+        # Register URLs
+
+        self.config['WolframAlpha']['appID'] = 'test123'
+
+        for question, answer, file in queries:
+            self.web.route(
+                url='http://api.wolframalpha.com/v2/query',
+                get={'input': question, 'appid': self.config['WolframAlpha']['appID']},
+                format='xml',
+                file=file
+            )
+
+    @Shaman.generate((query[:2] for query in queries))
+    def test(self, question, answer):
+        self.assertLooksLike(
+            self.query(question),
+            answer
+        )
